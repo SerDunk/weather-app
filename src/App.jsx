b82faa771e6fc7bd878f06c5adf39ffdff64357d
Abhinav Srivatsa Kondapalli <121254293+SerDunk@users.noreply.github.com>
2024-09-06 00:56:31 +0530

    Addes weather description based on weather code and temperature display

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,42 @@ import SearchBar from "./SearchBar";
 
 function App() {
 
+  const weatherCodes={
+    0: "Clear sky",
+    1: "Mainly clear",
+    2: "Partly cloudy",
+    3: "Overcast",
+    45: "Fog",
+    48: "Depositing rime fog",
+    51: "Drizzle: Light intensity",
+    53: "Drizzle: Moderate intensity",
+    55: "Drizzle: Dense intensity",
+    56: "Freezing Drizzle: Light intensity",
+    57: "Freezing Drizzle: Dense intensity",
+    61: "Rain: Slight intensity",
+    63: "Rain: Moderate intensity",
+    65: "Rain: Heavy intensity",
+    66: "Freezing Rain: Light intensity",
+    67: "Freezing Rain: Heavy intensity",
+    71: "Snow fall: Slight intensity",
+    73: "Snow fall: Moderate intensity",
+    75: "Snow fall: Heavy intensity",
+    77: "Snow grains",
+    80: "Rain showers: Slight intensity",
+    81: "Rain showers: Moderate intensity",
+    82: "Rain showers: Violent intensity",
+    85: "Snow showers: Slight intensity",
+    86: "Snow showers: Heavy intensity",
+    95: "Thunderstorm: Slight or moderate",
+    96: "Thunderstorm with slight hail",
+    99: "Thunderstorm with heavy hail"
+  }
+  
   const [term,setTerm]=useState("");
+
+  const [weatherDesc,setWeatherDesc]=useState("");
+  const [temp,setTemp]=useState(null);
+  
 
   const getCoordinates =async (term)=>{
     const coordinates=await GeoAPI(term);
@@ -15,19 +50,39 @@ function App() {
   }
   const getWeather = async (lat,lon)=>{
     const weather = await searchWeather(lat, lon);
+    const {weathercode}=weather;
+    const {temperature}=weather;
     console.log(weather)
+    return {weathercode,temperature};
   }
+
+
 
     const fetchWeather = async (term)=>{
       if (term){
-        const {lat,lng}=await getCoordinates(term);
-        await getWeather(lat,lng);
+        const coordinates=await getCoordinates(term);
+        if(coordinates==="Not Found"){
+          setWeatherDesc("Not Found");
+          setTemp(null)
+        }
+        else{
+          const weather=await getWeather(coordinates.lat,coordinates.lng);
+          const {weathercode,temperature}=weather;
+          const weatherDesc=weatherCodes[weathercode];
+          setTemp(temperature);
+          setWeatherDesc(weatherDesc);
+        }
+        
       }
 
     }
 
   
-  return <div><SearchBar term={term} setTerm={setTerm} onSubmit={fetchWeather} /></div>;
+  return <div>
+    <SearchBar term={term} setTerm={setTerm} onSubmit={fetchWeather} />
+  {weatherDesc}
+  {temp!==null && <div>{temp}°C</div>}
+  </div>;
 }
 
 export default App;
--- src/GeoAPI.jsx
+++ src/GeoAPI.jsx
@@ -1,16 +1,25 @@
 import axios from "axios";
 
 const GeoAPI = async (city) => {
-  const coordinates = await axios.get(
-    "https://api.opencagedata.com/geocode/v1/json",
-    {
-      params: {
-        q: city,
-        key: "9a000f52ae084210954ff71a1793e144",
-      },
+  try{
+    const coordinates = await axios.get(
+      "https://api.opencagedata.com/geocode/v1/json",
+      {
+        params: {
+          q: city,
+          key: "9a000f52ae084210954ff71a1793e144",
+        },
+      }
+    );
+    if(coordinates.data.results.length === 0){
+      return "Not Found";
     }
-  );
-  return coordinates.data.results[0].geometry;
-};
+    return coordinates.data.results[0].geometry;
+  }
+  catch(error){
+    return "Try later";
+  }
+}
+
 
 export default GeoAPI;
